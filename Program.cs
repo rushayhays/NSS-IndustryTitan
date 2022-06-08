8f4057a8273d9b159ec002ba46156eedd828f95e
rushayhays <rushayhays@outlook.com>
2022-06-08 10:07:13 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndustryTitan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
